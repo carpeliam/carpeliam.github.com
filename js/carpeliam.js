a0a34835ac79b1d9460faf09e10a33039b3c5472
Liam Morley <liam@carpeliam.com>
2013-11-17 06:25:34 -0500

    add dailymile to sidebar

--- js/carpeliam.js
+++ js/carpeliam.js
@@ -24,31 +24,32 @@ $(function() {
       if (typeof console !== 'undefined' && console !== null) {
         console.log('SoundCloud: ' + error.message);
       }
-      return;
-    }
-    var $player = $('<div>').insertAfter('#soundcloud');
-    var $list = $('<ul id="soundcloud_tracks">').insertAfter($player);
-    for (var i in tracks) {
-      var date = new Date(tracks[i].created_at);
-      var $track = $(sideItemTemplate({
-        title: tracks[i].title,
-        url: tracks[i].permalink_url,
-        time: date.toISOString(),
-        date: date.toString()
-      }));
-      $track.find('a').on('click', function(e) {
-        e.preventDefault();
-        SC.oEmbed($(this).attr('href'), { auto_play: true, color: '#5b8bc6', maxheight: 115, show_artwork: false }, function(oEmbed) {
-          $player.html(oEmbed.html);
+      $('<div class="error">').text('Unable to retrieve audio.').insertAfter('#soundcloud');
+    } else {
+      var $player = $('<div>').insertAfter('#soundcloud');
+      var $list = $('<ul id="soundcloud_tracks">').insertAfter($player);
+      for (var i in tracks) {
+        var date = new Date(tracks[i].created_at);
+        var $track = $(sideItemTemplate({
+          title: tracks[i].title,
+          url: tracks[i].permalink_url,
+          time: date.toISOString(),
+          date: date.toString()
+        }));
+        $track.find('a').on('click', function(e) {
+          e.preventDefault();
+          SC.oEmbed($(this).attr('href'), { auto_play: true, color: '#5b8bc6', maxheight: 115, show_artwork: false }, function(oEmbed) {
+            $player.html(oEmbed.html);
+          });
         });
-      });
-      $list.append($track);
+        $list.append($track);
+      }
+      $list.find('time').timeago();
     }
-    $list.find('time').timeago();
   });
   
   $.getJSON('http://feeds.delicious.com/v2/json/carpeliam?count=5&callback=?').done(function(data) {
-    var $list = $('<ul id="delicious_bookmarks"/>').insertAfter($('#delicious'));
+    var $list = $('<ul id="delicious_bookmarks">').insertAfter('#delicious');
     for (var i in data) {
       $list.append(sideItemTemplate({
         title: data[i].d,
@@ -58,7 +59,23 @@ $(function() {
       }));
     }
     $list.find('time').timeago();
-  }).fail(function(jqXHR, textStatus, errorThrown) {
-    $('<div class="error"/>').text('Unable to retrieve bookmarks.').insertAfter($('#delicious'));
+  }).fail(function() {
+    $('<div class="error">').text('Unable to retrieve bookmarks.').insertAfter('#delicious');
+  });
+
+  $.getJSON('https://api.dailymile.com/people/carpeliam/entries.json?callback=?').done(function(data) {
+    var $list = $('<ul id="dailymile_entries">').insertAfter('#dailymile');
+    for (var i in data.entries) {
+      var entry = data.entries[i];
+      $list.append(sideItemTemplate({
+        title: entry.workout.title,
+        url: entry.url,
+        time: entry.at,
+        date: $.timeago.parse(entry.at).toString()
+      }));
+    }
+    $list.find('time').timeago();
+  }).fail(function() {
+    $('<div class="error">').text('Unable to retrieve entries.').insertAfter('#dailymile');
   });
 });
