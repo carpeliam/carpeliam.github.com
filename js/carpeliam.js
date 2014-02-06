adebf0780caa486bf0ce2b9934c57520e899d77b
Liam Morley <liam@carpeliam.com>
2014-02-05 23:08:25 -0500

    add Backbone models/view for activity feed

--- js/carpeliam.js
+++ js/carpeliam.js
@@ -11,12 +11,70 @@ $(function() {
       $(this).closest("form").children("input[type=submit]").remove();
     }
   });
-  
+
   // tool tip
   $('nav a').popover({html: true, placement: 'top', trigger: 'hover focus'});
-  
+
   // side bar
-  var sideItemTemplate = _.template($('#sideItemTemplate').html());
+  var ModelItem = window.ModelItem = Backbone.Model.extend({
+    parse: function(attrs) {
+      for (var key in this.translations) {
+        var orig = this.translations[key];
+        attrs[key] = _(orig).isFunction() ? orig(attrs) : attrs[orig];
+      }
+      return attrs;
+    }
+  }),
+  SoundCloudItem = window.SoundCloudItem = ModelItem.extend({
+    translations: {url: 'permalink_url', date: function(attrs) { return new Date(attrs.created_at); }}
+  }),
+  LastFMItem = ModelItem.extend({
+    translations: {
+      title: function(attrs) { return attrs.artist['#text'] + ' &ndash; ' + attrs.name; },
+      date:  function(attrs) { return new Date(attrs.date.uts * 1000); }
+    }
+  }),
+  DeliciousItem = ModelItem.extend({
+    translations: {title: 'd', url: 'u', date: function(attrs) { return new Date(attrs.dt); }}
+  }),
+  DailyMileItem = ModelItem.extend({
+    translations: {title: function(attrs) { return attrs.workout.title; }, date: function(attrs) { return new Date(attrs.at); }}
+  }),
+  ItemCollection = Backbone.Collection.extend({
+    comparator: function(item) { return -item.get('date').getTime(); },
+    model: function(attrs, options) {
+      var types = {
+        soundcloud: SoundCloudItem,
+        lastfm: LastFMItem,
+        delicious: DeliciousItem,
+        dailymile: DailyMileItem
+      };
+      attrs.itemType = options.itemType;
+      var Item = types[attrs.itemType];
+      return new Item(attrs, options);
+    }
+  }),
+  ItemView = Backbone.View.extend({
+    tagName: 'ul',
+    className: 'activity-feed',
+    template: _.template($('#sideItemTemplate').html()),
+    initialize: function() {
+      this.collection.on('add', this.render, this);
+    },
+    render: function() {
+      this.$el.empty();
+      var view = this;
+      this.collection.each(function(item) {
+        var context = _.extend({}, item.attributes, {time: item.get('date').toISOString(), date: item.get('date').toString()});
+        view.$el.append(view.template(context));
+      });
+      this.$('time').timeago();
+      return this;
+    }
+  });
+  var sidebarItems = new ItemCollection, sidebarView = new ItemView({collection: sidebarItems});
+  $('#sidebar').append(sidebarView.render().el);
+
 
   SC.initialize({ client_id: '65e74ad7cc2b23eea893e18743d45f74' });
   SC.get('/users/6867700/tracks', {limit: 20}, function(tracks, error) {
@@ -24,58 +82,37 @@ $(function() {
       if (typeof console !== 'undefined' && console !== null) {
         console.log('SoundCloud: ' + error.message);
       }
-      $('<div class="error">').text('Unable to retrieve audio.').insertAfter('#soundcloud');
     } else {
-      var $player = $('<div>').insertAfter('#soundcloud');
-      var $list = $('<ul id="soundcloud_tracks">').insertAfter($player);
-      for (var i in tracks) {
-        var date = new Date(tracks[i].created_at);
-        var $track = $(sideItemTemplate({
-          title: tracks[i].title,
-          url: tracks[i].permalink_url,
-          time: date.toISOString(),
-          date: date.toString()
-        }));
-        $track.find('a').on('click', function(e) {
-          e.preventDefault();
-          SC.oEmbed($(this).attr('href'), { auto_play: true, color: '#5b8bc6', maxheight: 115, show_artwork: false }, function(oEmbed) {
-            $player.html(oEmbed.html);
-          });
-        });
-        $list.append($track);
-      }
-      $list.find('time').timeago();
+      sidebarItems.add(tracks, {parse: true, itemType: 'soundcloud'});
     }
   });
-  
-  $.getJSON('http://feeds.delicious.com/v2/json/carpeliam?count=5&callback=?').done(function(data) {
-    var $list = $('<ul id="delicious_bookmarks">').insertAfter('#delicious');
-    for (var i in data) {
-      $list.append(sideItemTemplate({
-        title: data[i].d,
-        url: data[i].u,
-        time: data[i].dt,
-        date: $.timeago.parse(data[i].dt).toString()
-      }));
+
+  var cache = new LastFMCache();
+  var lastfm = new LastFM({apiKey: 'b44f47f324f77489475e2a7542932da6', cache: cache});
+  lastfm.user.getRecentTracks({user:'carpeliam'}, {
+    success: function(data) {
+      sidebarItems.add(data.recenttracks.track, {parse: true, itemType: 'lastfm'});
+    },
+    error: function(code, message) {
+      if (typeof console !== 'undefined' && console !== null) {
+        console.log('SoundCloud: ' + message, code);
+      }
     }
-    $list.find('time').timeago();
+  });
+
+  $.getJSON('http://feeds.delicious.com/v2/json/carpeliam?count=10&callback=?').done(function(data) {
+    sidebarItems.add(data, {parse: true, itemType: 'delicious'});
   }).fail(function() {
-    $('<div class="error">').text('Unable to retrieve bookmarks.').insertAfter('#delicious');
+    if (typeof console !== 'undefined' && console !== null) {
+      console.log('Delicious: failure');
+    }
   });
 
   $.getJSON('https://api.dailymile.com/people/carpeliam/entries.json?callback=?').done(function(data) {
-    var $list = $('<ul id="dailymile_entries">').insertAfter('#dailymile');
-    for (var i in data.entries) {
-      var entry = data.entries[i];
-      $list.append(sideItemTemplate({
-        title: entry.workout.title,
-        url: entry.url,
-        time: entry.at,
-        date: $.timeago.parse(entry.at).toString()
-      }));
-    }
-    $list.find('time').timeago();
+    sidebarItems.add(data.entries, {parse: true, itemType: 'dailymile'});
   }).fail(function() {
-    $('<div class="error">').text('Unable to retrieve entries.').insertAfter('#dailymile');
+    if (typeof console !== 'undefined' && console !== null) {
+      console.log('DailyMile: failure');
+    }
   });
 });
